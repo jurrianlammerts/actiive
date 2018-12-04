8356a7dd7874ae211ca5ee376dccbd542cd686d9
Jurrian Lammerts <jurrian@chainels.com>
2018-12-04 21:22:54 +0100

    Made Single item page & fixed the delete bug & implemented pagination

--- sick-fits/backend/src/resolvers/Mutation.js
+++ sick-fits/backend/src/resolvers/Mutation.js
@@ -1,15 +1,19 @@
+
 const Mutations = {
   async createItem(parent, args, ctx, info) {
-    //TODO: check if logged in
+    // TODO: Check if they are logged in
 
     const item = await ctx.db.mutation.createItem(
       {
         data: {
-          ...args
-        }
+          ...args,
+        },
       },
       info
     );
+
+    console.log(item);
+
     return item;
   },
   updateItem(parent, args, ctx, info) {
@@ -22,12 +26,21 @@ const Mutations = {
       {
         data: updates,
         where: {
-          id: args.id
-        }
+          id: args.id,
+        },
       },
       info
     );
-  }
+  },
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{ id title}`);
+    // 2. Check if they own that item, or have the permissions
+    // TODO
+    // 3. Delete it!
+    return ctx.db.mutation.deleteItem({ where }, info);
+  },
 };
 
 module.exports = Mutations;
